ef534447897205d9c4d268356f12ccad5f38e87f
dkumazaw <dkumazaw@stanford.edu>
2020-05-13 01:45:30 -0700

    Init main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    match args.len() {
+        2 => {
+            println!(".intel_syntax noprefix");
+            println!(".global main\n");
+            println!("main:\n");
+            println!("  mov rax, {}\n", args[1]);
+            println!("  ret\n");
+        },
+        _ => {
+            eprintln!("Wrong number of arguments!");
+        }
+    }
 }
