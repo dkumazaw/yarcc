959846e4b734d2b714068185f8007d1ef51711f7
dkumazaw <dkumazaw@stanford.edu>
2020-05-13 02:08:22 -0700

    Create a file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::error::Error;
 use std::env;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     match args.len() {
         2 => {
+            let mut f = match File::create("tmp.s") {
+                Err(why) => panic!("Couldn't create tmp.s because {}", why.to_string()),
+                Ok(f) => f,
+            };
+
             println!(".intel_syntax noprefix");
             println!(".global main\n");
             println!("main:\n");
