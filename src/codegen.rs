6985e9ba9c5b375a8b24d38eca02ddab44fbc0a9
dkumazaw <dkumazaw26@gmail.com>
2020-05-20 02:05:25 +0000

    Init codegen

---
+++ src/codegen.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use crate::parser::Node;
+use crate::parser::NodeKind::*;
+use std::io::Write;
+
+macro_rules! gen_line {
+    ($dst:expr, $($arg: tt)*) => {
+        write!($dst, $($arg)*).unwrap()
+    }
+}
+
+pub fn gen(f: &mut File, node: Node) {
+    if node.kind == NDNUM {
+        gen_line!(f, "  push {}\n", node.val.unwrap());
+        return;
+    }
+    
+    if let Some(lhs) = node.lhs {
+        gen(f, *lhs);
+    }
+    if let Some(rhs) = node.rhs {
+        gen(f, *rhs);
+    }
+
+    gen_line!(f, "  pop rdi\n");
+    gen_line!(f, "  pop rax\n");
+
+    match node.kind {
+        NDADD => {
+            gen_line!(f, "  add rax, rdi\n");
+        }
+        NDSUB => {
+            gen_line!(f, "  sub rax, rdi\n");
+        }
+        NDMUL => {
+            gen_line!(f, "  imul rax, rdi\n");
+        }
+        NDDIV => {
+            gen_line!(f, "  cqo\n");
+            gen_line!(f, "  idiv rdi\n");
+        }
+        _ => {
+            panic!("Oops, found a strange node kind.")
+        }
+    }
+}
