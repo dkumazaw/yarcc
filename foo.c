ba23fb754d4048aee72e6150086803ffdb5489aa
dkumazaw <dkumazaw26@gmail.com>
2020-05-21 08:28:01 +0000

    Add example fns

--- foo.c
+++ foo.c
@@ -1,6 +1,18 @@
+
+// Test functions to be executed from generated binaries
 #include <stdio.h>
 
 int foo() {
     printf("OK\n");
+    return 1;
+}
+
+int foo1(int a) {
+    printf("%d OK\n", a);
+    return 1;
+}
+
+int foo6(int a, int b, int c, int d, int e, int f) {
+    printf("%d %d %d %d %d %d OK\n", a, b, c, d, e, f);
     return 1;
 }
