434fd1da4f5f9b6a089c8916c05fc990fdc9e664
dkumazaw <dkumazaw26@gmail.com>
2020-05-24 03:16:29 +0000

    Add another pointer test

--- tests/test.rs
+++ tests/test.rs
@@ -68,6 +68,7 @@ tests! {
     pointer1: ("int foo(int *aaa) { return *aaa; } int main() {int b; b = 120; return foo(&b); }", 120),
     pointer2: ("int main() { int x; int *xx; int **xxx; xx = &x; xxx = &xx; **xxx = 103; return x; } ", 103),
     pointer3: ("int main() { int x; int *y; y = &x; *y = 5; return *(&x); }", 5),
+    pointer4: ("int foo(int **x) { **x = 32; } int main() {int a; int *b; b = &a; foo(&b); return a; } ", 32),
     func0: ("int main() { return foo(); } 
              int foo() { return 123; }", 123),
     func1: ("int bar () { 24; } 
